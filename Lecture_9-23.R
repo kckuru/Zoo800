1b04252423c9ed4dc7265e671897f13c9d75b24a
Keeley Kuru <kckuru@gmail.com>
2025-09-23 14:48:10 -0500

    A new commit for Lecture Notes on 9/23

---
+++ Lecture_9-23.R
@@ -0,0 +1,15 @@
+################################
+###### Lecture Notes 9/23 ######
+################################
+
+
+## The expression function can be used for Greek letters 
+# Example: expression(Delta == 1)
+
+## Useful sources for plotting
+# --- A Compendium of Clean Graphs in R --- #
+# --- The R Graph Gallery --- #
+
+
+
+
