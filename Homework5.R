f94bdb40bc1c0a8fe043701ea446f3c2abb526d6
Keeley Kuru <kckuru@gmail.com>
2025-10-02 11:13:11 -0500

    Commit with Objective 1 complete.

--- Homework5.R
+++ Homework5.R
@@ -6,3 +6,36 @@
 # Date: 10/2/25
 
 # -- Objective 1 -- #
+setwd("/Users/keeleykuru/Documents/Kuru_Classwork/Zoo800/Kuru_Homework5")
+
+# Make R read an Excel file
+library(readxl)
+fish.xlsx <- read_excel("/Users/keeleykuru/Documents/Kuru_Classwork/Zoo800/Kuru_Homework5/fish.xlsx")
+fish.xlsx
+
+# Read a CSV file
+fish.csv <- read.csv("/Users/keeleykuru/Documents/Kuru_Classwork/Zoo800/Kuru_Homework5/fish.csv")
+fish.csv
+
+# Read R-specific format (.rds)
+fish.rds <- readRDS ("/Users/keeleykuru/Documents/Kuru_Classwork/Zoo800/Kuru_Homework5/fish.rds")
+fish.rds
+
+# Print first 5 rows of each dataset
+cat("First 5 rows of fish.xlsx:\n")
+print(head(fish.xlsx, 5))
+
+cat("\nFirst 5 rows of fish.csv:\n")
+print(head(fish.csv, 5))
+
+cat("\nFirst 5 rows of fish.rds:\n")
+print(head(fish.rds, 5))
+
+
+# -- Objective 2 -- #
+
+
+
+
+
+
