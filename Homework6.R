e5ec8d73025865ee0c0dd6380d1721d7294ad6b8
Keeley Kuru <kckuru@gmail.com>
2025-10-09 14:35:07 -0500

    New commit.

---
+++ Homework6.R
@@ -0,0 +1,8 @@
+##########################
+####### Homework 5 #######
+##########################
+
+# Group members: Keeley Kuru, Joseph Munoz and Lonnie Parry
+# Date: 10/9/25
+
+# -- Objective 1 -- #
